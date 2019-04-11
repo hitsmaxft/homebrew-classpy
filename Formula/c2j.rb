02f74628de4438d5072a5a98e3e81f2b612aadb5
gxk <liang.lu@duitang.com>
2019-04-11 16:05:01 +0800

    :tada: init

---
+++ Formula/c2j.rb
@@ -0,0 +1,10 @@
+class C2j < Formula
+  desc "convert csv to json"
+  homepage "https://github.com/guxingke"
+  url "https://github.com/guxingke/oss/raw/master/dist/c2j/c2j"
+  sha256 "4a91923f18d091d5f148cb2ff71c4a80d22d812cdb09f556213ba13672ecd6d0"
+
+  def install
+    bin.install "c2j"
+  end
+end
