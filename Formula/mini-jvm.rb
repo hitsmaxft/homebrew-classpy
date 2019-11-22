f5f14881c2d84bc18ed3234874deac8877bc3844
gxk <liang.lu@duitang.com>
2019-11-22 14:40:18 +0800

    .

--- Formula/mini-jvm.rb
+++ Formula/mini-jvm.rb
@@ -3,7 +3,7 @@ class MiniJvm < Formula
   homepage "https://github.com/guxingke/mini-jvm"
   url "https://github.com/guxingke/mini-jvm/releases/download/0.0.1-SNAPSHOT/mini-jvm"
   version "0.0.1"
-  sha256 "722b790043ab19127c61f45021c0b69bc6e29e9945b71af9594b8d457f9c307c"
+  sha256 "bfa2a818dbef39987067ce53a80a69dfaf684d0b7ecb3723f8b21a395379bb84"
 
   def install
     bin.install "mini-jvm"
