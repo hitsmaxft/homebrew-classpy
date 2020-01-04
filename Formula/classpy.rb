3ee85b44235f8f7d90a99df4fe6993d0a5901e4f
hitsmaxft <mfthits@gmail.com>
2020-01-04 13:49:19 +0800

    fix all

--- Formula/classpy.rb
+++ Formula/classpy.rb
@@ -1,9 +1,9 @@
 class Classpy < Formula
   desc "GUI tool for investigating Java class files"
   homepage "https://github.com/zxh0/classpy"
-  url "https://github.com/guxingke/oss/raw/master/dist/classpy/classpy-0.7.0.jar"
-  version "0.7.0"
-  sha256 "7155743c22e93f9adbbae0e8fcd3de70ce852f26ac1558e819a63109b770e033"
+  url "https://github.com/zxh0/classpy/releases/download/v0.7/classpy-0.7.jar"
+  version "0.7"
+  sha256 "a9d2cd870dc2486967173fbc4b1c522e8f4752a629789a100539d7022fd6c3ad"
 
   bottle :unneeded
 
